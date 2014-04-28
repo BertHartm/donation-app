51ccc80d15484828aa3493a41c638118f6fc57c8
Bert Hartmann <BertHartm@gmail.com>
2014-04-28 19:44:29 -0400

    add a map of donations

--- app/assets/javascripts/donors.js
+++ app/assets/javascripts/donors.js
@@ -1,3 +1,4 @@
+'use strict';
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
@@ -5,15 +6,31 @@ $(document).ready(function() {
     $(".donation-type-form").hide();
 
     $('input#donation_type_physical').on("click", function() {
-	$(".donation-type-form").hide();
-	$("#physical").show();
+        $(".donation-type-form").hide();
+        $("#physical").show();
     });
     $('input#donation_type_voucher').on("click", function() {
-	$(".donation-type-form").hide();
-	$("#voucher").show();
+        $(".donation-type-form").hide();
+        $("#voucher").show();
     });
     $('input#donation_type_experience').on("click", function() {
-	$(".donation-type-form").hide();
-	$("#experience").show();
+        $(".donation-type-form").hide();
+        $("#experience").show();
     });
 });
+
+function initializeMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(40.7127, -74.0059),
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    experience_donations.forEach(function(el, idx, arr) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(el.donation_type.latitude, el.donation_type.longitude),
+            map: map,
+            title: el.title
+        });
+    });
+}
+google.maps.event.addDomListener(window, 'load', initializeMap);
